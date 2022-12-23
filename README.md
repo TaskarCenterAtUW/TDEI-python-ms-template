02f344fc5c11dad7785d5128aeb49a81416db155
Sujata <sujatam@gaussiansolutions.com>
2022-12-23 21:04:59 +0530

    Initial Fast APIs including some sample implementation of python_ms_core

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # TDEI-Python-Template-MS
 ## Introduction 
 This micro-service acts as example for creating all the new micro-services for TDEI project in Python. The reference code can be used in other micro-services.
+
+To run the project
+
+`uvicorn src.main:app --reload `
---
+++ src/main.py
@@ -0,0 +1,34 @@
+from typing import Union
+from fastapi import FastAPI
+from fastapi.responses import Response
+from python_ms_core import Core
+
+app = FastAPI()
+
+
+class OctetStreamResponse(Response):
+    media_type = 'application/octet-stream'
+
+
+@app.get('/')
+def root():
+    return {'Hello': 'World'}
+
+
+@app.get('/ping')
+def ping():
+    return {'msg': 'Ping Successful'}
+
+
+@app.get('/download', response_class=OctetStreamResponse)
+async def download():
+    storage_client = Core.get_storage_client()
+    storage_container = storage_client.get_container(name='gtfspathways')
+    file_lists = storage_container.list_files()
+    first_file = file_lists[1]
+    return Response(
+        content=first_file.get_stream(),
+        media_type=first_file.mimetype,
+        headers={'Content-Disposition': f'attachment; filename={first_file.name}'},
+        status_code=200
+    )
